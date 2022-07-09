2036e5e697a61b4bfea69a0e52139b4cef7c418f
Artemka0-7 <pavlovskiy.07@inbox.ru>
2022-07-09 18:54:14 +0300

    helmet

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,42 @@
-import { Fragment } from 'react'
-import 'aos/dist/aos.css';
-import NavBar from './components/navbar/Navbar'
-import Home from './components/home/Home'
-import Skills from './components/skills/Skills'
-import Portfolio from './components/portfolio/Portfolio'
-import Reviews from './components/reviews/Reviews'
-import Contacts from './components/contacts/Contacts'
-import Footer from './components/footer/Footer'
-import AOS from 'aos';
+import "aos/dist/aos.css";
+import NavBar from "./components/navbar/Navbar";
+import Home from "./components/home/Home";
+import Skills from "./components/skills/Skills";
+import Portfolio from "./components/portfolio/Portfolio";
+import Reviews from "./components/reviews/Reviews";
+import Contacts from "./components/contacts/Contacts";
+import Footer from "./components/footer/Footer";
+import AOS from "aos";
+import Helmet from "react-helmet";
 
 const App = () => {
   AOS.init();
   return (
-    <Fragment>
-      <div className='bg'>
-        <div className='bg-circle-1'></div>
-        <div className='bg-circle-2'></div>
-        <div className='bg-circle-3'></div>
-        <div className='bg-circle-4'></div>
+    <>
+      <Helmet
+        title="FaxWeb"
+        meta={[
+          {
+            'name': "description",
+            'content':
+              "FaxWeb - начинающий Front-End разработчик, делающий крутые сайты.",
+          },
+          {
+            'name': "keywords",
+            'content':
+              "FaxWeb, faxweb, faxweb артем, Артем Павловский, Артем, Павловский, артем, артем павловский, Артем Павловский Разработчик, Front-End Developer, Back-End-Developer, React, Node.js, Next.js",
+          },
+          {
+            'name': "Author",
+            'content': "Артём Павловский"
+          }
+        ]}
+      />
+      <div className="bg">
+        <div className="bg-circle-1"></div>
+        <div className="bg-circle-2"></div>
+        <div className="bg-circle-3"></div>
+        <div className="bg-circle-4"></div>
       </div>
       <NavBar />
       <div className="wrapper">
@@ -28,8 +47,7 @@ const App = () => {
         <Contacts />
         <Footer />
       </div>
-    </Fragment>
-    
+    </>
   );
 };
 
